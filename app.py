825b7c924bbb2792774e3261b00359efef5ffb7b
Arnold Farkas <farkasarnold1981@gmail.com>
2018-02-21 17:22:38 +0100

    Revert "separate get and post"
    This reverts commit 646b3b243c3866e6b49056704df74021a1f023f3.

--- app.py
+++ app.py
@@ -8,32 +8,31 @@ ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 bot = Bot (ACCESS_TOKEN)
 
-
-@app.route("/", methods=['GET'])
-def verify_token():
-    """Before allowing people to message your bot, Facebook has implemented a verify token
-    that confirms all requests that your bot receives came from Facebook."""
-    token_sent = request.args.get("hub.verify_token")
-    return verify_fb_token(token_sent)
-
-
-# get whatever message a user sent the bot
-@app.route("/", methods=['POST'])
+#We will receive messages that Facebook sends our bot at this endpoint 
+@app.route("/", methods=['GET', 'POST'])
 def receive_message():
-   output = request.get_json()
-   for event in output['entry']:
-      messaging = event['messaging']
-      for message in messaging:
-        if message.get('message'):
-            #Facebook Messenger ID for user so we know where to send response back to
-            recipient_id = message['sender']['id']
-            if message['message'].get('text'):
-                response_sent_text = get_message()
-                send_message(recipient_id, response_sent_text)
-            #if user sends us a GIF, photo,video, or any other non-text item
-            if message['message'].get('attachments'):
-                response_sent_nontext = get_message()
-                send_message(recipient_id, response_sent_nontext)
+    if request.method == 'GET':
+        """Before allowing people to message your bot, Facebook has implemented a verify token
+        that confirms all requests that your bot receives came from Facebook.""" 
+        token_sent = request.args.get("hub.verify_token")
+        return verify_fb_token(token_sent)
+    #if the request was not get, it must be POST and we can just proceed with sending a message back to user
+    else:
+        # get whatever message a user sent the bot
+       output = request.get_json()
+       for event in output['entry']:
+          messaging = event['messaging']
+          for message in messaging:
+            if message.get('message'):
+                #Facebook Messenger ID for user so we know where to send response back to
+                recipient_id = message['sender']['id']
+                if message['message'].get('text'):
+                    response_sent_text = get_message()
+                    send_message(recipient_id, response_sent_text)
+                #if user sends us a GIF, photo,video, or any other non-text item
+                if message['message'].get('attachments'):
+                    response_sent_nontext = get_message()
+                    send_message(recipient_id, response_sent_nontext)
     return "Message Processed"
 
 
@@ -47,7 +46,7 @@ def verify_fb_token(token_sent):
 
 #chooses a random message to send to the user
 def get_message():
-    sample_responses = ["Well done!!", "Yeah, Yeah!!"]
+    sample_responses = ["Well done!", "Yeah, Yeah!"]
     # return selected item to the user
     return random.choice(sample_responses)
 
