6712dced4c1e70b69923caa721f28082872e8c18
Arnold Farkas <farkasarnold1981@gmail.com>
2018-03-03 22:21:22 +0100

    8

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ def verify_fb_token(token_sent):
 
 def send_message(opt, recipient_id, response):
     if response.startswith(opt.IMAGE_PREFIX):
+        print("bot.send_image", recipient_id, response[len(opt.IMAGE_PREFIX):])
         bot.send_image(recipient_id, response[len(opt.IMAGE_PREFIX):])
     else:
         bot.send_text_message(recipient_id, response)
